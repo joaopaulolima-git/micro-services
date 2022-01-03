72e08434d62bf7b02401478e523b68058b94820c
João Paulo <jplopapa.git@gmail.com>
2022-01-03 13:52:49 -0300

    Project hr-api-gateway-zuul created

---
+++ hr-apigateway-zuul/src/test/java/com/jpdev/hrapigatewayzuul/HrApigatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpdev.hrapigatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApigatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
