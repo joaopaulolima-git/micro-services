b378a40ea1f45816db3809173e687c3fcc74ac76
João Paulo <jplopapa.git@gmail.com>
2022-01-03 14:50:52 -0300

    Project hr-config-server created

---
+++ hr-config-server/src/main/java/com/jpdev/hrconfgserver/HrConfgServerApplication.java
@@ -0,0 +1,13 @@
+package com.jpdev.hrconfgserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrConfgServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrConfgServerApplication.class, args);
+	}
+
+}
---
+++ hr-config-server/src/test/java/com/jpdev/hrconfgserver/HrConfgServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpdev.hrconfgserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrConfgServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
